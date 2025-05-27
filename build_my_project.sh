f2674f2ef2de54dcc53d11d7ca0b8fae116847ef
Joraslav <yar.mozg2002@gmail.com>
2025-05-27 23:05:33 +0300

    Add install targets in CMake

---
+++ build_my_project.sh
@@ -0,0 +1,13 @@
+set -e
+
+BUILD_DIR="build"
+INSTALL_DIR="$HOME/miniparser_install"
+
+rm -rf "$BUILD_DIR"
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+
+cmake .. -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR"
+cmake --build . --target install
+
+echo "Installed to $INSTALL_DIR"
